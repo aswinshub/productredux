8e8cb06891898c384156f94849d0db8192caf9ed
aswinshub <aswin1998ply@gmail.com>
2024-03-04 21:46:37 +0530

    btn

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import products from '../../api/products.json'
-import BeforeCart from './cartButtons/BeforeCart'
-import AfterCart from './cartButtons/AfterCart'
-import '../ProductList/ProductList.css'
-
-
-
+import React, { useState } from "react";
+import products from "../../api/products.json";
+import BeforeCart from "./cartButtons/BeforeCart";
+import AfterCart from "./cartButtons/AfterCart";
+import "../ProductList/ProductList.css";
 
 const ProductList = () => {
-  return <section className='container'>
-{products.map((products,key)=>(
-<div className="product-container" key={key}>
+  const [count, setCount] = useState(0);
 
-<img src={products.image} alt="" />
-<h3>{products.title}</h3>
+  const addToCart = () => {
+    setCount(1);
+  };
 
-<BeforeCart/>
-<AfterCart/>
+  return (
+    <section className="container">
+      {products.map((products, key) => (
+        <div className="product-container" key={key}>
+          <img src={products.image} alt="" />
+          <h3>{products.title}</h3>
+{count>0 ? <AfterCart /> :<BeforeCart addToCart={addToCart} />}
+          
+        
+        </div>
+      ))}
+    </section>
+  );
+};
 
-
-</div>
-
-))}
-
-  </section>
-}
-
-export default ProductList
+export default ProductList;
--- src/components/ProductList/cartButtons/BeforeCart.js
+++ src/components/ProductList/cartButtons/BeforeCart.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const BeforeCart = () => {
+const BeforeCart = ({addToCart}) => {
   return (
     <div  className="before-cart">
-    <button className="add-cart-button" >
+    <button className="add-cart-button" onClick={addToCart} >
       Add to cart
    </button>
 </div>
